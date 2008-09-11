e2a1c89d9e3228b122a306ebf59d2f2d19f1fd54
m0 <m0@localhost>
2008-09-11 17:01:39 +0100

    Compiler warning 'fixed'.

--- UNIT_TESTS/open.adb
+++ UNIT_TESTS/open.adb
@@ -1,13 +1,20 @@
 with utest;
 with lua;
 
+-- the majority of strange code here is to stop very strict compilers from
+-- complaining about unread or unreferenced variables.
 procedure open is
+  use type lua.state_ptr_t;
+
   ls: lua.state_ptr_t;
   lsdummy: lua.state_ptr_t;
 begin
   ls := lua.open;
-  lsdummy := ls; -- stop very strict compilers from complaining about unread var
+  lsdummy := ls;
   ls := lsdummy;
+  if ls /= lsdummy then
+    raise program_error with "assignment error";
+  end if;
 exception
   when others => utest.fail (2, "unexpected exception");
 end open;
